d1b13fa7e066cedfda512113f81d6038583c430d
Elvis <1904313468@qq.com>
2018-12-11 18:54:42 +0800

    Initial commit

---
+++ src/main/java/com/shiroproject/shirogateway/ShirogatewayApplication.java
@@ -0,0 +1,12 @@
+package com.shiroproject.shirogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirogatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShirogatewayApplication.class, args);
+	}
+}
